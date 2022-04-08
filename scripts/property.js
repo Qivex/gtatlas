b6b5f0bd43cfd6a34adb8c7e5c469eb539c4436a
Qivex <Qivex@web.de>
2022-04-08 18:06:03 +0200

    Make parent group use correct reference to child (update on mount)

--- scripts/property.js
+++ scripts/property.js
@@ -2,20 +2,55 @@ const templateList = ["property", "propertygroup"]
 
 
 // Create v-scope for Property
-function Property(context) {
+function Property(context, parent) {
+	// Keep original object, don't use copy
 	Object.assign(context, {
 		$template: "#property-template",
 		state: (context.initial === undefined ? false : context.initial),
-		nextState: function() {this.state = !this.state}
+		nextState: function() {this.state = !this.state},	//Todo: Update parent group state recusively
+		logState: function() {console.log(this.state)},
+		isReactive: function() {return this !== this.self},	//Equal by default, but broken after used in v-scope!
+		self: context,
+		parent: parent,
+		updateParentRef: function() {
+			/* Explanation:
+			- When used as scope this context gets reactified
+			- But parents ref isn't updated -> still points at non-reactified version of this
+			- Therefore updates don't propagate properly (non-reactified version gets updated)
+			- So we have to update the ref manually after mounting
+			*/
+			let index = this.parent.children.indexOf(this.self)
+			this.parent.children[index] = this
+		}
 	})
 	return context
 }
 
 
 // Create v-scope for PropertyGroup
-function PropertyGroup(context) {
+function PropertyGroup(context, parent) {
+	context = Property(context)	//Reuse most of the props & functions
+	context.$template = "#propertygroup-template"
+	/*
 	Object.assign(context, {
-		$template: "#propertygroup-template"
+		$template: "#propertygroup-template",
+		_state: false,
+		nextState: function() {this.state = !this.state},
+		logState: function() {console.log(this.state)},
+		isReactive: function() {return this !== this.self},
+		self: context,
+		parent: parent
+	})
+	*/
+	Object.defineProperty(context, "state", {
+		set: function(newstate) {
+			this._state = newstate
+			this.children.forEach(child => {
+				child.state = newstate
+				//console.log(child.isReactive())
+			})
+		},
+		get: function() {return this._state}
 	})
 	return context
 }
