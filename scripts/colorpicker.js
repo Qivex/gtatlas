8bcdd0948482978e72bd17b86fba0a2d8e0e7958
Qivex <Qivex@web.de>
2022-05-06 18:25:15 +0200

    Implemented ColorPicker

--- scripts/colorpicker.js
+++ scripts/colorpicker.js
@@ -10,37 +10,58 @@ var ColorPicker = {
 	name: "ColorPicker",
 	template: template,
 	props: {
-		colors: Array,
+		colors: Array,	// TODO: Flawed calculation if length=1
+		gapRatio: Number,
 		onchange: Function
 	},
-	data: function() {
+	data() {
 		return {
-			selectedIndex: 0
+			selectedIndex: 0,
+			totalSize: 100	// Inccurate positioning if too small (like 1)!
 		}
 	},
 	computed: {
-		pathRadius: function() {
-			
+		ratio() {
+			// Ratio of inner circle to outer circle.
+			// Using regular polygon to calculate:
+			// ratio = half side length / circumradius
+			return Math.sin(Math.PI / this.colors.length)
 		},
-		circleRadius: function() {
-			
+		pathRadius() {
+			// Split total size according to ratio:
+			// totalSize = 2*(pathRadius + circleRadius)
+			return this.totalSize / (2*this.ratio + 2)
+		},
+		circleRadius() {
+			// Apply calculated ratio -> largest possible
+			let r = this.pathRadius * this.ratio
+			// Apply gap
+			if (this.gapRatio) {
+				r *= 1 - this.gapRatio
+			}
+			return r
+		},
+		pathCommand() {
+			// Shortcuts
+			let s = this.totalSize
+			let r1 = this.pathRadius
+			let r2 = this.circleRadius
+			// Split into 2 halfs arcs of circle (relative 0,0 isnt drawn)
+			return `M${s/2},${r2} a${r1},${r1} 0,1,1 0,${2*r1} a${r1},${r1} 0,1,1 0,${-2*r1}`
 		}
 	},
 	methods: {
-		selectColor: function(index) {
+		selectColor(index) {
 			if (this.selectedIndex != index) {
 				this.selectedIndex = index
 				this.onchange(this.colors[index])
 			}
 		},
-		getKeyPoints(index) {
+		keyPoints(index) {
+			// Hack to position element at any position of arbitrary path:
+			// animateMotion with calcMode=discrete and static keyPoints for 0;1
 			let part = index / this.colors.length
 			return part + ';' + part
-		},
-		getRadius() {
-			let n = this.colors.length
-			const r = 48
-			return r * Math.sin(Math.PI / n)
 		}
 	}
 }
