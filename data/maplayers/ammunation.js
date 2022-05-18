4b1e3264094769febd83fe518fd13a33eae1a726
Qivex <qivex@web.de>
2022-05-18 13:57:07 +0200

    Started Popup

--- data/maplayers/ammunation.js
+++ data/maplayers/ammunation.js
@@ -1,8 +1,8 @@
 let coords = [
-	[5000,5000],
-	[5200,5000]
+	[4000,6000],
+	[4200,6000]
 ]
 
 export default function (Leaflet, marker) {
-	return Leaflet.featureGroup(coords.map(pixel => marker(pixel[0], pixel[1], "ammunation")))
+	return Leaflet.featureGroup(coords.map(pixel => marker(pixel[0], pixel[1], "ammunation"))).bindPopup("<p>Test</p>")
 }
