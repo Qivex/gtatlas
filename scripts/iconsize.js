fb77639f07d1a74e2e02a1841ebf228a7ff5290e
Qivex <Qivex@web.de>
2022-06-20 10:32:23 +0200

    Started resizable icons

---
+++ scripts/iconsize.js
@@ -0,0 +1,23 @@
+var IconSizeSelector = {
+	name: "IconSizeSelector",
+	template: templates["iconsize"],
+	data() {
+		return {
+			size: 35
+		}
+	},
+	watch: {
+		size: function() {
+			
+		}
+	},
+	methods: {
+		updatePreview: function() {
+		
+		},
+		updateMap: function() {
+		
+		}
+	}
+	
+}
